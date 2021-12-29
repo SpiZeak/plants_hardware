7bbc6993e3583ea03c784bbb1433f5c652782f54
Max Trewhitt <max.trewhitt@osolo.se>
2021-12-29 14:02:16 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
 // Define the Firebase Data object
 FirebaseData fbdo;
 
@@ -14,14 +17,12 @@ FirebaseAuth auth;
 // Define the FirebaseConfig data for config data
 FirebaseConfig config;
 
-void tokenStatusCallback(TokenInfo info);
-
-const int sense_Pin = 35;
-int moisture_value = 0;
+const int sensePin = 35;
+int moistureValue = 0;
 
 void setup()
 {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Disable brownout detector
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable   detector
 
   WiFiManager wifiManager;
   wifiManager.setCountry("SE");
@@ -51,7 +52,7 @@ void setup()
   Serial.println("Connecting to Firebase");
   Firebase.begin(&config, &auth);
 
-  Serial.print("Waiting for token");
+  Serial.print("Waiting for token... ");
 
   while (Firebase.authTokenInfo().status != token_status_ready)
   {
@@ -59,12 +60,14 @@ void setup()
     delay(300);
   }
 
-  moisture_value = analogRead(sense_Pin);
+  Serial.println("Done.");
 
-  if (Firebase.RTDB.setInt(&fbdo, "plants/plant_1/sensor", moisture_value))
+  moistureValue = analogRead(sensePin);
+
+  if (Firebase.RTDB.setInt(&fbdo, "plants/plant_1/sensor", moistureValue))
   {
-    Serial.println("Values successfully updated");
-    Serial.println(moisture_value);
+    Serial.println("Values successfully updated:");
+    Serial.println(moistureValue);
   }
   else
   {
@@ -77,85 +80,4 @@ void setup()
 
 void loop()
 {
-}
-
-void tokenStatusCallback(TokenInfo info)
-{
-  if (info.status == token_status_error)
-  {
-    Serial.printf("Token info: type = %s, status = %s\n", getTokenType(info).c_str(), getTokenStatus(info).c_str());
-    Serial.printf("Token error: %s\n", getTokenError(info).c_str());
-  }
-  else
-  {
-    Serial.printf("Token info: type = %s, status = %s\n", getTokenType(info).c_str(), getTokenStatus(info).c_str());
-  }
-}
-
-/* The helper function to get the token type string */
-String getTokenType(struct token_info_t info)
-{
-  switch (info.type)
-  {
-  case token_type_undefined:
-    return "undefined";
-
-  case token_type_legacy_token:
-    return "legacy token";
-
-  case token_type_id_token:
-    return "id token";
-
-  case token_type_custom_token:
-    return "custom token";
-
-  case token_type_oauth2_access_token:
-    return "OAuth2.0 access token";
-
-  default:
-    break;
-  }
-  return "undefined";
-}
-
-/* The helper function to get the token status string */
-String getTokenStatus(struct token_info_t info)
-{
-  switch (info.status)
-  {
-  case token_status_uninitialized:
-    return "uninitialized";
-
-  case token_status_on_initialize:
-    return "on initializing";
-
-  case token_status_on_signing:
-    return "on signing";
-
-  case token_status_on_request:
-    return "on request";
-
-  case token_status_on_refresh:
-    return "on refreshing";
-
-  case token_status_ready:
-    return "ready";
-
-  case token_status_error:
-    return "error";
-
-  default:
-    break;
-  }
-  return "uninitialized";
-}
-
-/* The helper function to get the token error string */
-String getTokenError(struct token_info_t info)
-{
-  String s = "code: ";
-  s += String(info.error.code);
-  s += ", message: ";
-  s += info.error.message.c_str();
-  return s;
 }
